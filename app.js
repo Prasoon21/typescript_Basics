9a38f3f5b94c244f825094802e1cae1dcec00445
Prasoon21 <prashun.parashar_cs19@gla.ac.in>
2024-02-05 12:01:44 +0530

    Typescript with node and express

--- app.js
+++ app.js
@@ -1,29 +1,12 @@
-var num1Ele = document.getElementById('num1');
-var num2Ele = document.getElementById('num2');
-var buttonEle = document.querySelector('button');
-var numResults = [];
-var stringResults = [];
-function add(num1, num2) {
-    if (typeof num1 === 'number' && typeof num2 === 'number') {
-        return num1 + num2;
-    }
-    else if (typeof num1 === 'string' && typeof num2 === 'string') {
-        return num1 + ' ' + num2;
-    }
-    return +num1 + +num2;
-}
-function printResult(resultObj) {
-    console.log(resultObj.val);
-}
-if (buttonEle) {
-    buttonEle.addEventListener('click', function () {
-        var num1 = num1Ele.value;
-        var num2 = num2Ele.value;
-        var result = add(+num1, +num2);
-        numResults.push(result);
-        var stringResult = add(num1, num2);
-        stringResults.push(stringResult);
-        printResult({ val: result, timestamp: new Date() });
-        console.log(numResults, stringResults);
-    });
-}
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const body_parser_1 = __importDefault(require("body-parser"));
+const todos_1 = __importDefault(require("./routes/todos"));
+const app = (0, express_1.default)();
+app.use(body_parser_1.default.json());
+app.use(todos_1.default);
+app.listen(3000);
